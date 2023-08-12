63ad530383af266e24f42cb8adf4f9f717ac3b13
Ankit Patwa <ankitpatwa2141@gmail.com>
2023-08-12 10:30:21 +0530

    Update mongoose.js

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-mongoose.connect('mongodb://localhost/habitTracker',{useNewUrlParser:true})
+mongoose.connect('mongodb+srv://Coolest:QVpsSMZaPbWTtalo@habittraker.m3ze8oc.mongodb.net/HabitTraker?retryWrites=true&w=majority',{useNewUrlParser:true})
 
 const db = mongoose.connection;
 
